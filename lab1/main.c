4d4832cf5b35021285ae5ec1693650e6856a0287
takjekrasiva <takjekrasiva@icloud.com>
2019-03-11 18:55:14 +0300

    Create main.c

---
+++ lab1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define g 10
+int main(void)
+{
+	float H;
+	float V;
+	float t;
+	printf("H=");
+	scanf("%f", &H);
+	printf("t=");
+	scanf("%f", &t);
+	V = h/t + gt/2;
+	printf("skorost=%f\n",V);
+	return 0;
+}
