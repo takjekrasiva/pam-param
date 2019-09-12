eb1f316d4282d6fe9940c46701405219b9dc9951
takjekrasiva <47566712+takjekrasiva@users.noreply.github.com>
2019-09-12 22:30:20 +0300

    Update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -1,40 +1,30 @@
 # include <stdio.h>
-# define MAXLINE 1000 // максимальная длина строки в потоке 
+# define MAXLINE 1000
 void main(void)
 {
-	char line[MAXLINE]; // текущая максимальная длина
-	int i = 0; // позиция текущего символа исходной  cтроки
+	char line[MAXLINE];
+	int start;
+	int cnt = 0;
+	int i = 0;
 	int maximum = 0;
-	int symb = 0; // для защиты от ЦИФЕРЕК
-	int cnt = 0; // счётчик
-	int start; // позиция начала слова
 	gets(line);
 	while (line[i] != '\0') // поиск максимальной длины слова и его начала
 	{
-		if ((line[i] != ' ') && (line[i] != '.') && (line[i] != ',') && (line[i] != '\n') && (line[i] != '\0')) // найден разделитель
+		if ((line[i] != ' ') && (line[i] != '.') && (line[i] != ','))
 		{
-			if (symb == 0) //были ли в слове лишние символы, если нет, выполить...
-			{ cnt = 0; // т.к. слово закончилось, обнуляем счетчик букв в слове
-			symb = 0; // т.к. слово закончилось, обнуляем переменную лишних символов
-		}
-		else
 			cnt++;
 			if (cnt > maximum)
 			{
 				maximum = cnt;
-				start = i - maximum + 1; // запомнить позицию
- // начала слова
- // проверка совпадения текущего и
- // предыдущего символов
- // (обнаружение искомого признака в слове)
+				start = i - maximum + 1;
 			}
 		}
-		     else
+		else
 			cnt = 0;
 		i++;
 	}
 	i = start;
-	while (i < (MAXLINE - maximum)) //перезапись
+	while (i < (MAXLINE - maximum)) // перезапись
 	{
 		line[i] = line[i + maximum];
 		i++;
