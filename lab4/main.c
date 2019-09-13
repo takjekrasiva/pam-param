50796ed5fae9d91e7f68214bbc9487dc8030d16e
takjekrasiva <47566712+takjekrasiva@users.noreply.github.com>
2019-09-14 00:21:37 +0300

    Update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -1,33 +1,33 @@
 # include <stdio.h>
 # define MAXLINE 1000
-void main (void)
+void main(void)
 {
-    char line[LEN];
-    int i=0;
-    int start;
-    int cnt=0;
-    int maximum=0;
-    gets(line);
-    while (line[i]!='\0') // поиск максимальной длины слова и его начала
-    {
-        if ((line[i]!=' ') && (line[i]!='.') && (line[i]!=','))
-        {
-                cnt++;
-                if (cnt>maximum)
-            {
-                max=cnt;
-                start=i-maximum+1;
-            }
-        }
-            else
-                counter=0;
-        i++;
-    }
-    i=start;
-    while (i<(MAXLINE-maximum)) //перезапись
-       {
-           line[i]=line[i+maximum];
-           i++;
-    }
-    puts (line);
+	char line[MAXLINE];
+	int i = 0;
+	int start;
+	int cnt = 0;
+	int maximum = 0;
+	gets(line);
+	while (line[i] != '\0') // поиск максимальной длины слова и его начала
+	{
+		if ((line[i] != ' ') && (line[i] != '.') && (line[i] != ','))
+		{
+			cnt++;
+			if (cnt > maximum)
+			{
+				max = cnt;
+				start = i - maximum + 1;
+			}
+		}
+		else
+			cnt = 0;
+		i++;
+	}
+	i = start;
+	while (i < (MAXLINE - maximum)) //перезапись
+	{
+		line[i] = line[i + maximum];
+		i++;
+	}
+	puts(line);
 }
