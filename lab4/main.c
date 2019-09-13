29fde3c9418fe73269fe426d1e086c507bff1059
takjekrasiva <47566712+takjekrasiva@users.noreply.github.com>
2019-09-13 23:32:20 +0300

    Update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -1,14 +1,21 @@
+ning(disable : 4996)
 # include <stdio.h>
 # define MAXLINE 1000
 void main(void)
 {
+	printf("please, enter some words \n");
 	char line[MAXLINE];
 	int start;
 	int cnt = 0;
 	int i = 0;
 	int maximum = 0;
 	gets(line);
-	while (line[i] != '\0') // поиск максимальной длины слова и его начала
+	if ((line[i] < 'a' || line[i] > 'z') && (line[i] < 'A' || line[i] > 'Z')) // проверить не является ли этот символ лишним
+	{
+		puts(line); // в слове есть лишний символ
+	}
+	else
+		while (line[i] != '\0') // поиск максимальной длины слова и его начала
 	{
 		if ((line[i] != ' ') && (line[i] != '.') && (line[i] != ','))
 		{
@@ -16,18 +23,18 @@ void main(void)
 			if (cnt > maximum)
 			{
 				maximum = cnt;
-				start = i - maximum + 1;
+				start = line[i] - maximum + 1;
 			}
 		}
 		else
 			cnt = 0;
-		i++;
+		line[i] ++;
 	}
-	i = start;
-	while (i < (MAXLINE - maximum)) // перезапись
+	line[i] = start;
+	while (line[i] i < (MAXLINE - maximum)) // перезапись
 	{
 		line[i] = line[i + maximum];
-		i++;
+		line[i] ++;
 	}
 	puts(line);
 }
